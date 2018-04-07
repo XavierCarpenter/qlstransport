8ef333cdce6b15d571306172e949756571c37848
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-04-06 23:49:13 -0500

    setup structure of website

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="App">
+        <nav>
+          <ul>
+            <li>About Us</li>
+            <li>Apply Now</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+        <div className="box a">
+          <div className="title-box">
+            <h1 className="big-text">DESIGNED TO RESPOND</h1>
+            <h1 className="big-text">BUILT TO DELIVER</h1>
+          </div>
+          <div>
+            <p>
+              A Safety First Transportation Solutions Provider. This is
+              Landstar.
+            </p>
+            <button>Contact Us</button>
+          </div>
+        </div>
+        <div className="box b">
+          <div className="title-box">
+            <h1 className="big-text">OWNER</h1>
+            <h1 className="big-text">OPERATORS</h1>
+          </div>
+          <div>
+            <p>
+              <span className="bold">
+                With Landstar, you’re in control.
+              </span> You decide what and when to haul and our advanced technology ensures you're only a click away from the right choice for you. You've never known freedom like this before.
+            </p>
+          </div>
+          <button>Learn More</button>
+        </div>
+        <div className="box c">
+          <div>
+            {" "}
+            <p>
+              Landstar gives you the freedom to succeed on your own terms
+              without any blood, sweat or tears. Explore our interactive
+              diagram to find out more.
+            </p>
+          </div>
+        </div>
+        <div className="box d">D</div>
+      </div>;
   }
 }
 
