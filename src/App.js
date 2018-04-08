800c86c61bfe4c94a7c244af4ddd2f7e423aa8d4
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-04-08 18:14:07 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      learn: false
+    };
+  }
+  learnActive() {
+    this.setState({ learn: true });
+  }
+  backActive() {
+    this.setState({ learn: false });
+  }
   render() {
     return <div className="App">
         <nav>
           <ul>
-            <li>About Us</li>
+            <li><a href="#about">About Us</a></li>
             <li>|</li>
-            <li>Apply Now</li>
+            <li><a>Apply Now</a></li>
             <li>|</li>
-            <li>Contact Us</li>
+            <li><a href="#contact">Contact Us</a></li>
           </ul>
         </nav>
-        <div className="box intro">
+        <div className="box intro" >
           <div className="title-box">
             <h1 className="big-text">DESIGNED TO RESPOND</h1>
             <h1 className="big-text">BUILT TO DELIVER</h1>
@@ -27,7 +39,7 @@ class App extends Component {
             <button className="btn btn1">Contact Us</button>
           </div>
         </div>
-        <div className="box about">
+        <div className="box about" id="about">
           <div className="title-box">
             <h1 className="big-text">OWNER</h1>
             <h1 className="big-text">OPERATORS</h1>
@@ -36,39 +48,88 @@ class App extends Component {
             <p className="subtext">
               <span className="bold">
                 With Landstar, you’re in control.
-              </span> You decide what and when to haul and our advanced technology ensures you're only a click away from the right choice for you. You've never known freedom like this before.
+              </span>
+              You decide what and when to haul and our advanced technology ensures you're only a click away from the right choice for you. You've never known freedom like this before.
             </p>
-          </div>
-          <button className="btn btn2">Learn More</button>
-        </div>
-        <div className="box c">
-          <div className="sub-box">
-            <p className="subtext">
-              Landstar gives you the freedom to succeed on your own terms
-              without any blood, sweat or tears. Explore our interactive
-              diagram to find out more.
-            </p>
+            <button onClick={() => this.learnActive()} className="btn btn2">
+              Learn More
+            </button>
           </div>
         </div>
-        <div className="box d">
+        {this.state.learn == false ? <div className="box c">
+            <div className="sub-box">
+              <p className="subtext2">
+                Landstar gives you the freedom to succeed on your own terms
+                without any blood, sweat or tears. Explore our interactive
+                diagram to find out more.
+              </p>
+            </div>
+          </div> : <div className="box p">
+            <div className="extra-box">
+              <div className="infoBox">
+                <p className="subtext3">
+                  <span className="bold">
+                    With Landstar, you can grow your business
+                  </span> on your own terms. You choose the loads that meet your business needs. You go home when you want and head back out when you’re ready, plus with Landstar’s percentage pay, as rates go up, so do your settlements.
+                </p>
+              </div>
+              <div className="infoBox">
+                <h2 className="bluetext">FREEDOM: BE YOUR OWN BOSS</h2>
+                <p className="subtext3">
+                  With Landstar you are your own boss, you choose what you
+                  want to haul, when to haul and when to return home. We
+                  won't get in your way.
+                </p>
+              </div>
+              <div className="infoBox">
+                <h2 className="bluetext">
+                  SECURITY: YOU'LL ALWAYS BE IN DEMAND
+                </h2>
+                <p className="subtext3">
+                  There are thousands of loads available every day. There's
+                  always something waiting for you.
+                </p>
+              </div>
+              <div className="infoBox">
+                <h2 className="bluetext">
+                  SUPPORT: WE’LL HELP YOU ALL THE WAY
+                </h2>
+                <p className="subtext3">
+                  Landstar is committed to your success. We'll help you get
+                  to where you're going - because that helps us get to where
+                  we're going.
+                </p>
+              </div>
+              <p className="subtext3 bold">
+                Call us today for more details at 855-578-7226.
+              </p>
+              <button onClick={() => this.learnActive()} className="btn btn2">
+                Apply Now
+              </button>
+              <button onClick={() => this.backActive()} className="btn btn2">
+                Back
+              </button>
+            </div>
+          </div>}
+        <div className="box d" id="contact">
           <div className="title-box">
             <h1 className="big-text">CONTACT</h1>
           </div>
         </div>
         <div className="box e">
-        <div className="info-wrapper">
-          <div className="name-box">
-            <h2>QLS Transport – Corporate Headquarters/Dallas</h2>
-          </div>
-          <div className="contact-box">
-            <ul>
-            <li>13410 Sutton Park Drive, South</li>
-            <li>Jacksonville, Florida 32224</li>
-            <li>817-438-0865</li>
-            <li>natalieamos@qlstransport.com</li>
-            </ul>
-            <i class="fab fa-facebook-square" />
-          </div>
+          <div className="info-wrapper">
+            <div className="name-box">
+              <h2>QLS Transport – Corporate Headquarters/Dallas</h2>
+            </div>
+            <div className="contact-box">
+              <ul>
+                <li>13410 Sutton Park Drive, South</li>
+                <li>Jacksonville, Florida 32224</li>
+                <li>817-438-0865</li>
+                <li>natalieamos@qlstransport.com</li>
+              </ul>
+              <i class="fab fa-facebook-square" />
+            </div>
           </div>
         </div>
       </div>;
