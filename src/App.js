a9af97d0c2510d4023c5d322c8dcd26bfb964834
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-04-16 23:20:19 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      learn: false
+      learn: false,
+      nav: false
     };
   }
   learnActive() {
@@ -14,6 +15,12 @@ class App extends Component {
   }
   backActive() {
     this.setState({ learn: false });
+  }
+  navActive(){
+    this.setState({ nav: true})
+  }
+  navInactive(){
+    this.setState({ nav: false});
   }
   render() {
     return <div className="App">
@@ -34,6 +41,33 @@ class App extends Component {
             </li>
           </ul>
         </nav>
+        <div className="navBars">
+          <i onClick={() => this.navActive()} className="fas fa-bars" />
+        </div>
+        {this.state.nav === true ? <div className="dropdown">
+            <div className="x">
+              <i onClick={() => this.navInactive()} class="fas fa-times" />
+            </div>
+            <ul>
+              <li>
+                <a onClick={() => this.navInactive()} href="#about">
+                  About Us
+                </a>
+              </li>
+              <li>|</li>
+              <li>
+                <a onClick={() => this.navInactive()} href="https://docs.google.com/forms/d/e/1FAIpQLSecrAMb603zUDU73IxPmUBSDGVkaBj6bBC1_3Sb_k_Cf5aEyQ/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
+                  Apply Now
+                </a>
+              </li>
+              <li>|</li>
+              <li>
+                <a onClick={() => this.navInactive()} href="#contact">
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+          </div> : null}
         <div className="box intro">
           <div className="title-box">
             <h1 className="big-text">DESIGNED TO RESPOND</h1>
@@ -41,10 +75,12 @@ class App extends Component {
           </div>
           <div className="sub-box">
             <p className="subtext">
-              A Safety First Transportation Solutions Provider. This is
-              QLS Transport.
+              A Safety First Transportation Solutions Provider. This is QLS
+              Transport.
             </p>
-            <button className="btn btn1">Contact Us</button>
+            <button className="btn btn1">
+              <a href="#contact">Contact Us</a>
+            </button>
           </div>
         </div>
         <div className="box about" id="about">
@@ -64,15 +100,15 @@ class App extends Component {
             </button>
           </div>
         </div>
-        {this.state.learn === false ? <div className="box c" id="changeHeader">
+        {this.state.learn === false ? <div className="box about2" id="changeHeader">
             <div className="sub-box">
               <p className="subtext2">
-                QLS Transport gives you the freedom to succeed on your own terms
-                without any blood, sweat or tears. Explore our interactive
-                diagram to find out more.
+                QLS Transport gives you the freedom to succeed on your own
+                terms without any blood, sweat or tears. Explore our
+                interactive diagram to find out more.
               </p>
             </div>
-          </div> : <div className="box p">
+          </div> : <div className="box about3">
             <div className="extra-box">
               <div className="infoBox">
                 <p className="subtext3">
@@ -84,8 +120,8 @@ class App extends Component {
               <div className="infoBox">
                 <h2 className="bluetext">FREEDOM: BE YOUR OWN BOSS</h2>
                 <p className="subtext3">
-                  With QLS Transport you are your own boss, you choose what you
-                  want to haul, when to haul and when to return home. We
+                  With QLS Transport you are your own boss, you choose what
+                  you want to haul, when to haul and when to return home. We
                   won't get in your way.
                 </p>
               </div>
@@ -103,9 +139,9 @@ class App extends Component {
                   SUPPORT: WE’LL HELP YOU ALL THE WAY
                 </h2>
                 <p className="subtext3">
-                  QLS Transport is committed to your success. We'll help you get
-                  to where you're going - because that helps us get to where
-                  we're going.
+                  QLS Transport is committed to your success. We'll help you
+                  get to where you're going - because that helps us get to
+                  where we're going.
                 </p>
               </div>
               <p className="subtext3 bold">
@@ -122,20 +158,67 @@ class App extends Component {
               </button>
             </div>
           </div>}
-        <div className="box d" id="contact" />
-        <div className="box e">
+          <div className="box pMobile">
+            <div className="extra-box">
+              <div className="infoBox">
+                <p className="subtext3">
+                  <span className="bold">
+                    With QLS Transport, you can grow your business
+                  </span> on your own terms. You choose the loads that meet your business needs. You go home when you want and head back out when you’re ready, plus with Landstar’s percentage pay, as rates go up, so do your settlements.
+                </p>
+              </div>
+              <div className="infoBox">
+                <h2 className="bluetext">FREEDOM: BE YOUR OWN BOSS</h2>
+                <p className="subtext3">
+                  With QLS Transport you are your own boss, you choose what
+                  you want to haul, when to haul and when to return home. We
+                  won't get in your way.
+                </p>
+              </div>
+              <div className="infoBox">
+                <h2 className="bluetext">
+                  SECURITY: YOU'LL ALWAYS BE IN DEMAND
+                </h2>
+                <p className="subtext3">
+                  There are thousands of loads available every day. There's
+                  always something waiting for you.
+                </p>
+              </div>
+              <div className="infoBox">
+                <h2 className="bluetext">
+                  SUPPORT: WE’LL HELP YOU ALL THE WAY
+                </h2>
+                <p className="subtext3">
+                  QLS Transport is committed to your success. We'll help you
+                  get to where you're going - because that helps us get to
+                  where we're going.
+                </p>
+              </div>
+              <p className="subtext3 bold">
+                Call us today for more details at 855-578-7226.
+              </p>
+              <h2 onClick={() => this.learnActive()} >
+                {" "}
+                <a  href="https://docs.google.com/forms/d/e/1FAIpQLSecrAMb603zUDU73IxPmUBSDGVkaBj6bBC1_3Sb_k_Cf5aEyQ/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
+                  Apply Now
+                </a>
+              </h2>
+           
+            </div>
+          </div>
+        <div className="box contactPic" />
+        <div className="box contact" id="contact">
           <div className="title-box">
-            <h1 className="big-text contact">CONTACT</h1>
+            <h1 className="big-text contact" id="cTitle">CONTACT</h1>
           </div>
           <div className="info-wrapper">
             <div className="info-box">
               <h2>Headquarters</h2>
-                <p>Dallas, Texas</p>
-                <h2>Phone</h2>
-                <p>817-438-0865</p>
-                <h2>Email</h2>
-                <p>natalieamos@qlstransport.com</p>
-          
+              <p>Dallas, Texas</p>
+              <h2>Phone</h2>
+              <p>817-438-0865</p>
+              <h2>Email</h2>
+              <p>natalieamos@qlstransport.com</p>
             </div>
           </div>
         </div>
